6b51b0570b3f5260ec1a81d6247a99f6876712e4
GitHub Action <action@github.com>
2022-11-29 11:26:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/fuyuaaa/enumcodegenerate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/fuyuaaa/enumcodegenerate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fuyuaaa.enumcodegenerate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.fuyuaaa.enumcodegenerate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
